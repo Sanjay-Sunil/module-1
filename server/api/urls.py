1f58f26002515e18f469205340b55e3dc4a1aa9e
[Sanjay Sunil] <sanjay25bcd07@iiitkottayam.ac.in>
2026-03-06 23:56:51 +0530

    matchingmaking from an array of interest to a list of users with top to bottom overlap decreasing

--- server/api/urls.py
+++ server/api/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import user_list_create, update_delete
+from .views import user_list_create, update_delete, match_user_by_overlap
 
 urlpatterns = [
   path('users/', user_list_create, name='user_list_create'),
   path('users/<int:id>', update_delete, name="edit_user" ),
+  path('match/', match_user_by_overlap, name="match_users")
 ]
+
--- server/api/views.py
+++ server/api/views.py
@@ -2,17 +2,15 @@ from django.shortcuts import render
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
-
+from django.db.models import Count, Q
 from .models import Users
 from .serializers import UserSerializer
 
-
+# creating user , get and post
 @api_view(['GET', 'POST'])
 def user_list_create(request):
-  serializer = UserSerializer(data = request.data)
-  
-  
-  if request.method == 'POST': 
+  if request.method == 'POST':
+    serializer = UserSerializer(data = request.data)
     if serializer.is_valid():
       serializer.save()
       return Response(serializer.data, status=status.HTTP_201_CREATED)
@@ -24,7 +22,7 @@ def user_list_create(request):
     serializer = UserSerializer(users, many=True)
     return Response(serializer.data)
 
-
+# updating or deleting users, put and delete
 @api_view(['PUT', 'DELETE'])
 def update_delete(request, id):
   try:
@@ -34,10 +32,31 @@ def update_delete(request, id):
   
   if request.method == 'DELETE':
     user.delete()
-    return Response(status=status.HTTP_202_ACCEPTED)
+    return Response(status=status.HTTP_204_NO_CONTENT)
+  
   elif request.method == 'PUT':
     data = request.data
     serializer = UserSerializer(user, data=data)
-    serializer.save()
-    return Response(serializer.data)
+    if serializer.is_valid():
+      serializer.save()
+      return Response(serializer.data)
   return Response(serializer.error, status=status.HTTP_400_BAD_REQUEST)
+
+
+# the matching process
+
+@api_view(['POST'])
+def match_user_by_overlap(request):
+  interests = request.data.get('interests', [])
+  
+  if not interests:
+    return Response({'error', "No keywords Provided"}, status=status.HTTP_400_BAD_REQUEST)
+  
+  matched_users = Users.objects.filter(
+    interests__name__in = interests
+  ).annotate(
+    overlap_count = Count('interests', filter=Q(interests__name__in=interests))
+  ).order_by('-overlap_count')
+  
+  serializer = UserSerializer(matched_users, many=True)
+  return Response(serializer.data)
